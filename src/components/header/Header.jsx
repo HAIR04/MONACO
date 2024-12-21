074cc55168ba5a5c11b8ed4859753727fc692b80
DangHoangHai <danghoanghai2907@gmail.com>
2024-12-21 14:32:41 +0700

    ok

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,11 +2,20 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Modal } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpaceAwesome } from "@fortawesome/free-brands-svg-icons";
+import {
+  faUser,
+  faBarsStaggered,
+  faRankingStar,
+  faCircleNodes,
+} from "@fortawesome/free-solid-svg-icons";
 import ContactInfo from "./ContactInfo";
 import Subscribe_Responsive from "../subscribe/Subscribe_Responsive";
 import Booking from "../../components/subscribe/Subscribe";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
   const [isOverlayVisible, setIsOverlayVisible] = useState(false);
   const [selectedImageIndex, setSelectedImageIndex] = useState(0);
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -16,7 +25,7 @@ const Header = () => {
   };
 
   return (
-    <div className="mx-auto justify-between bg-black border-opacity-15 w-[95%] md:w-[90%] 2xl:w-[100%] xl:w-[93%]">
+    <div className="mx-auto justify-between flex items-center overflow-hidden lg:flex-col bg-black border-opacity-15 w-[95%] md:w-[90%] 2xl:w-[100%] xl:w-[93%]">
       <ContactInfo />
 
       <div className="w-[90%] mx-auto flex justify-between items-center">
@@ -34,44 +43,43 @@ const Header = () => {
         </div>
 
         <div className="w-[70%] animate-goldShine">
-        <ul className="2xl:w-[100%] hidden justify-around items-center xl:flex text-white text-2xl font-semibold xl:w-[100%]">
-  <li>
-    <Link
-      to="/"
-      className="relative inline-block text-red-700  transition-all duration-300 after:content-[''] after:block after:w-0 after:h-1 after:bg-red-800 after:transition-all after:duration-300 hover:after:w-full"
-    >
-      TRANG CHỦ
-    </Link>
-  </li>
-  <li>
-    <Link
-      to="/service"
-      className="relative inline-block  animate-goldShine  transition-all duration-300 after:content-[''] after:block after:w-0 after:h-1 after:bg-yellow-600 after:transition-all after:duration-300 hover:after:w-full"
-    >
-      DỊCH VỤ
-    </Link>
-  </li>
-  <li>
-    <Link
-      to="/rankings"
-      className="relative inline-block  animate-goldShine  transition-all duration-300 after:content-[''] after:block after:w-0 after:h-1 after:bg-yellow-600 after:transition-all after:duration-300 hover:after:w-full"
-    >
-      KỸ THUẬT VIÊN
-    </Link>
-  </li>
-  <li>
-    <Link
-      to="/wallet"
-      className="relative inline-block  animate-goldShine  transition-all duration-300 after:content-[''] after:block after:w-0 after:h-1 after:bg-yellow-600 after:transition-all after:duration-300 hover:after:w-full"
-    >
-      LIÊN HỆ
-    </Link>
-  </li>
-</ul>
-
+          <ul className="2xl:w-[100%] hidden justify-around items-center xl:flex text-white text-2xl font-semibold xl:w-[100%]">
+            <li>
+              <Link
+                to="/"
+                className="relative inline-block text-red-700  transition-all duration-300 after:content-[''] after:block after:w-0 after:h-1 after:bg-red-800 after:transition-all after:duration-300 hover:after:w-full"
+              >
+                TRANG CHỦ
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/service"
+                className="relative inline-block  animate-goldShine  transition-all duration-300 after:content-[''] after:block after:w-0 after:h-1 after:bg-yellow-600 after:transition-all after:duration-300 hover:after:w-full"
+              >
+                DỊCH VỤ
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/rankings"
+                className="relative inline-block  animate-goldShine  transition-all duration-300 after:content-[''] after:block after:w-0 after:h-1 after:bg-yellow-600 after:transition-all after:duration-300 hover:after:w-full"
+              >
+                KỸ THUẬT VIÊN
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/wallet"
+                className="relative inline-block  animate-goldShine  transition-all duration-300 after:content-[''] after:block after:w-0 after:h-1 after:bg-yellow-600 after:transition-all after:duration-300 hover:after:w-full"
+              >
+                LIÊN HỆ
+              </Link>
+            </li>
+          </ul>
         </div>
 
-        <div className=" w-[15%]" onClick={toggleOverlay}>
+        <div className=" w-[15%] hidden lg:block" onClick={toggleOverlay}>
           <Booking />
         </div>
         {isOverlayVisible && (
@@ -82,6 +90,49 @@ const Header = () => {
             <Subscribe_Responsive setOpen={setIsModalOpen} />
           </div>
         )}
+      </div>
+
+      <div>
+        <button
+          onClick={() => setOpen(!open)}
+          className="xl:hidden text-2xl text-white  "
+        >
+          <FontAwesomeIcon icon={faBarsStaggered} className="text-yellow-700" />
+          {open && (
+            <div className="fixed w-[50%] top-0  right-0 bottom-0 bg-black z-50 flex flex-col ">
+              <div className="bg-body py-3 text-right">MENU</div>
+              <Link
+                to="/"
+                className="text-lg border-b text-red-600 border-white border-opacity-30 p-3 w-full text-right font-medium"
+              >
+                TRANG CHỦ
+              </Link>
+              <Link
+                to="/service"
+                className="text-lg border-b border-white animate-goldShine  transition-all duration-300  after:bg-yellow-600 after:transition-all after:duration-300 hover:after:w-full border-opacity-30 p-3 w-full text-right font-medium"
+              >
+                 DỊCH VỤ
+              </Link>
+
+              <Link
+                to="/rankings"
+                className="text-lg border-b border-white animate-goldShine  transition-all duration-300  after:bg-yellow-600 after:transition-all after:duration-300 hover:after:w-full border-opacity-30 p-3 w-full text-right font-medium"
+              >
+               
+                KỸ THUẬT VIÊN
+              </Link>
+              <Link
+                to="/wallet"
+                className="text-lg border-b border-white animate-goldShine  transition-all duration-300  after:bg-yellow-600 after:transition-all after:duration-300 hover:after:w-full border-opacity-30 p-3 w-full text-right font-medium"
+              >
+              LIÊN HỆ
+              </Link>
+              <div className="  block lg:hidden" onClick={toggleOverlay}>
+          <Booking />
+        </div>
+            </div>
+          )}
+        </button>
       </div>
     </div>
   );
